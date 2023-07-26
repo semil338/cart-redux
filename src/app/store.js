253488eec80813dc5226bd1760a316af43e76bbb
Semil Khedawala <semilkhedawala@gmail>
2023-07-26 15:48:18 +0530

    reducer config added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "../features/product/productSlice";
+import cartReducer from "../features/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    product: productReducer,
+    cart: cartReducer,
   },
 });
